83cfaf35e0ed0d1480f7bcd1533723bc2f4c8d74
Marusa09Marusa <mariami.karkarashvili09@gmail.com>
2024-10-07 23:50:24 +0400

    Create homework.js

---
+++ homework.js
@@ -0,0 +1,16 @@
+var name = "Mariami";
+console.log(name); // Mariami
+console.log(typeof name); // string 
+
+
+const PI = 3.14;
+console.log(PI); // 3.14
+console.log(typeof PI); // number 
+
+
+age = 26;
+console.log(age); // 26
+console.log(typeof age); // number 
+
+
+console.log(age*PI);
