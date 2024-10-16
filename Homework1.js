126d0cb9df2bc2616be5dd11a81f9e581bc2e5cb
Marusa09Marusa <mariami.karkarashvili09@gmail.com>
2024-10-16 20:30:49 +0400

    Update Homework1.js

--- Homework1.js
+++ Homework1.js
@@ -1,16 +1,20 @@
+// 1 
 var name = "Mariami";
 console.log(name); // Mariami
 console.log(typeof name); // string 
 
 
+// 2 
 const PI = 3.14;
 console.log(PI); // 3.14
 console.log(typeof PI); // number 
 
 
+// 3 
 age = 26;
 console.log(age); // 26
 console.log(typeof age); // number 
 
 
+// 4 
 console.log(age*PI);
