59bb2edd55769441625c3f8ff7cde1e91e8b2f64
Marusa09Marusa <mariami.karkarashvili09@gmail.com>
2024-10-16 20:23:41 +0400

    Create Homework2.js

---
+++ Homework2.js
@@ -0,0 +1,55 @@
+// 1 
+let num = 0;
+
+if (num > 0) {
+console.log ('greater than 0')}
+else if (num < 0) {
+console.log ('is less than 0');}
+else {
+console.log ('is equal to 0');}
+
+
+// 2 
+let message = num % 2 === 0 ? "even" : "odd";
+console.log (message);
+
+
+// 3
+let grade = "D";
+
+switch (grade) {
+case "A": console.log ('Excellent');
+break;
+case "B": console.log ('Good');
+break;
+case "C": console.log ('Average');
+break;
+case "D": console.log ('Bad');
+break;
+case "F": console.log ('Fail');
+break;} 
+
+
+// 4
+
+
+
+
+
+
+
+// 5
+let i = 1;
+
+while (i < 100) {
+console.log (i);
+i*=2;}
+
+
+// 6
+
+
+
+
+
+// 7
